67360eec659aaa9c962c7bde2e34259c758703db
DarkMKH <MVCoren@gmail.com>
2015-04-27 14:41:13 +0100

    Added basic GUI script

---
+++ Unity Project/Assets/Scripts/GUIScripts/MainGUIScript.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainGUIScript : MonoBehaviour
+{
+	// provisional numbers
+	int menuWidth = 250;
+	int menuHeight = 350; 
+
+	// button variables
+	int buttonWidth = 200;
+	int buttonHeight = 30;
+	int buttonHeightOffset = 45;
+
+	// dependant on the window's size
+	int relativeStartWidth;
+	int relativeStartHeight;
+	int startButtonHeight;
+
+	void OnGUI ()
+	{
+		relativeStartWidth = Screen.width / 2 - menuWidth / 2;
+		relativeStartHeight = Screen.height / 2 - menuHeight / 2;
+		startButtonHeight = relativeStartHeight + buttonHeightOffset;
+
+		// main box
+		GUI.Box ( new Rect ( relativeStartWidth, relativeStartHeight, menuWidth, menuHeight ), "Main Menu" );
+
+		// button for "New Game"
+		if ( GUI.Button ( new Rect ( relativeStartWidth, startButtonHeight, buttonWidth, buttonHeight ), "New Game" ))
+		{
+
+		}
+
+		// button for "Load Game"
+		if ( GUI.Button ( new Rect ( relativeStartWidth, startButtonHeight + (buttonHeightOffset), buttonWidth, buttonHeight ), "Load Game" ))
+		{
+			// purposefully empty
+		}
+	}
+}
