b9f759bc1decc78fb4a46e2de0266a0b9f9ed9fe
Wolfie13 <wwwolfie13@gmail.com>
2015-04-27 15:18:42 +0100

    Missed Commit
    .

--- Unity Project/Assets/Terrain/TerrainMeshGenerator.cs
+++ Unity Project/Assets/Terrain/TerrainMeshGenerator.cs
@@ -13,38 +13,119 @@ public class TerrainMeshGenerator : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
 	}
 
 	private const int HORIZONTAL_RES = 128;
 	private const int VERTICAL_RES = 128;
+
+	private Vector3[] start = {new Vector3(-0.5f, 0.5f, 0.5f), new Vector3(-0.5f, 0.5f, -0.5f), new Vector3(-0.5f, -0.5f, 0.5f), new Vector3(-0.5f, -0.5f, -0.5f)};
+
+	private Vector3[] end = {new Vector3(0.5f, 0.5f, 0.5f), new Vector3(0.5f, 0.5f, -0.5f), new Vector3(0.5f, -0.5f, 0.5f), new Vector3(0.5f, -0.5f, -0.5f)};
+
+	private static void AddWithOffset (List<Vector3> list, Vector3[] items, Vector3 offset)
+	{
+		foreach (Vector3 v in items)
+		{
+			list.Add(v + offset);
+		}
+	}
+
+	private static void AddUVs (List<Vector2> list, Vector2 uv)
+	{
+		list.Add (uv);
+		list.Add (uv);
+		list.Add (uv);
+		list.Add (uv);
+	}
+
+	private static void AddTriangles (List<int> tris, int lastPoint)
+	{
+		//[start/end, top/bottom, back/front]
+		int ebb = lastPoint;
+		int ebf = lastPoint - 1;
+		int etb = lastPoint - 2;
+		int etf = lastPoint - 3;
+		int sbb = lastPoint - 4;
+		int sbf = lastPoint - 5;
+		int stb = lastPoint - 6;
+		int stf = lastPoint - 7;
+
+		//Front plane
+		tris.Add (sbf);
+		tris.Add (ebf);
+		tris.Add (etf);
+		tris.Add (etf);
+		tris.Add (stf);
+		tris.Add (sbf);
+
+		//Top Plane
+		tris.Add (stf);
+		tris.Add (stb);
+		tris.Add (etb);
+		tris.Add (etb);
+		tris.Add (etf);
+		tris.Add (stf);
+
+	}
+
 	private enum GeneratorState {RUNNING, CLEAR};
 	void UpdateMesh()
 	{
+		Mesh mesh = new Mesh ();
+		this.terrainMesh.mesh = mesh;
+		//Set up our storage for mesh data.
 		List<Vector3> verts = new List<Vector3> ();
 		List<int> triangles = new List<int> ();
 		List<Vector2> uvs = new List<Vector2>();
-		GeneratorState state = GeneratorState.CLEAR;
-		int startPoint = -1;
-		for (int x = 0; x != HORIZONTAL_RES; x++) {
+
+		for (int y = 0; y != VERTICAL_RES; y++) {
+			//Set up the state machine used for the mesh generator.
 			GeneratorState genState = GeneratorState.CLEAR;
-			for (int y = 0; x != VERTICAL_RES; y++) {
+			for (int x = 0; x != HORIZONTAL_RES; x++) {
+				Vector3 basePos = new Vector3(x, y, 0);
+
 				Vector2 uv = new Vector2((1 / HORIZONTAL_RES) * x, (1 / VERTICAL_RES) * y);
 				Color c = terrainMap.GetPixelBilinear(uv.x, uv.y);
-				//We don't need to do anything if we're not making a strip.
 
 				if (genState == GeneratorState.RUNNING) {
-					if (c.a == 0.0) {
+					if (c.a < 0.1) {
 						//Transition to the CLEAR state, build the end of this strip.
-					}
-				}
-				else {
-					if (c.a == 0.0) {
+						AddWithOffset(verts, end, basePos);
+						AddUVs(uvs, uv);
+						AddTriangles(triangles, verts.Count);
+						genState = GeneratorState.CLEAR;
+					} else {
+						//Do nothing.
 						continue;
 					}
 				}
+				else if (genState == GeneratorState.CLEAR) {
+					if (c.a < 0.1) {
+						//We don't need to do anything if we're not making a strip.
+						continue;
+					} else {
+						//Build the start of a strip.
+						AddWithOffset(verts, start, basePos);
+						AddUVs(uvs, uv);
+						genState = GeneratorState.RUNNING;
+					}
 
+				}
 			}
+			if (genState == GeneratorState.RUNNING) {
+				Vector3 basePos = new Vector3(y, HORIZONTAL_RES, 0);
+				Vector2 uv = new Vector2(1, (1 / VERTICAL_RES) * y);
+
+				AddWithOffset(verts, end, basePos);
+				AddUVs(uvs, uv);
+				AddTriangles(triangles, verts.Count);
+				genState = GeneratorState.CLEAR;
+			}
+
 		}
+
+		mesh.vertices = verts.ToArray ();
+		mesh.triangles = triangles.ToArray ();
+		mesh.uv = uvs.ToArray ();
 	}
 }
