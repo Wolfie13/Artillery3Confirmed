a5952a2cdd897af0e54b891a4a720ede2e01f5ba
ScoredOne <scoredone1994@gmail.com>
2015-05-06 11:51:57 +0100

    shooting
    blagh

---
+++ Unity Project/Assets/Scripts/Shooting.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shooting : MonoBehaviour {
+
+	public GameObject FirePoint;
+	public GameObject Weapon;
+	public int firepower;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown ("x")) {
+			GameObject progectile = (GameObject)Instantiate (Weapon, FirePoint.transform.position, FirePoint.transform.rotation);
+			progectile.rigidbody.AddForce (FirePoint.transform.forward * firepower);
+		}
+	}
+}
