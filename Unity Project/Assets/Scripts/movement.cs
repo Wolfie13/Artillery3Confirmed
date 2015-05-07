68392b1602178901f49f3526091e34f52ae0eb4c
Ruddy92 <n3281997@SCM-69328.windows.tees.ac.uk>
2015-05-08 00:01:55 +0100

    done basic movement

---
+++ Unity Project/Assets/Scripts/movement.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class movement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetKey (KeyCode.A))
+						transform.position += Vector3.left * Time.deltaTime;
+		if (Input.GetKey (KeyCode.D))
+						transform.position += Vector3.right * Time.deltaTime;
+	
+
+	}
+
+
+
+}
+
