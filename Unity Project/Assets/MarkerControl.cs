309ee5f5172f3c899efb15c4cf2a0d3107f11304
Wolfie13 <wwwolfie13@gmail.com>
2015-05-13 10:39:05 +0100

    New Marker Prefab/Logic

---
+++ Unity Project/Assets/MarkerControl.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MarkerControl : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.gameObject.transform.Rotate (new Vector3 (0, Time.deltaTime * 40.0f, 0));
+	}
+}
