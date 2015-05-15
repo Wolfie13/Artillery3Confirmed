9a489c7cd698448236f9734569be67876bb980b5
ScoredOne <scoredone1994@gmail.com>
2015-05-15 10:30:42 +0100

    menu stuff (not done)
    stuff with menu included still needs work

---
+++ Unity Project/Assets/menu stuff/Materials/ButtonsStuff.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonsStuff : MonoBehaviour {
+
+	public Material one;
+	public Material two;
+	public Renderer rend;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnMouseEnter() {
+		rend.material = two;
+	}
+
+	void OnMouseExit() {
+		rend.material = one;
+	}
+
+}
---
+++ Unity Project/Assets/menu stuff/OptionsScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OptionsScript : MonoBehaviour {
+
+	public Material one;
+	public Material two;
+	public Renderer rend;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
